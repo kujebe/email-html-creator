1d7c990a58bc37a28919fb8cf9b3d72f580aaacd
kujebe <kujebeseun@gmail.com>
2020-08-09 15:23:42 +0100

    Replaced html with pug

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const browserSync = require("browser-sync").create();
 const inlineCss = require("gulp-inline-css");
 const replace = require("gulp-replace");
 const rename = require("gulp-rename");
+const pug = require("gulp-pug");
 
 //function to compile scss into css
 function compileStyles() {
@@ -27,11 +28,14 @@ function build() {
     gulp
       // import all email template (name ending with .template.pug) files from src/emails folder
       //1 Where are the html files located
-      .src("src/templates/**/*.html")
+      .src("src/templates/**/*.template.pug")
 
       // replace `.scss` file paths from template with compiled file paths
       //2 Rename the extension of the scss files included in the htmls
       .pipe(replace(new RegExp("/scss/(.+).scss", "ig"), "/css/$1.css"))
+
+      // compile pug files
+      .pipe(pug())
 
       //3 inline the styles
       .pipe(inlineCss())
@@ -58,7 +62,7 @@ function watch() {
   );
   //3 watch changes in the templates directory, then rebuild and refresh browser
   gulp
-    .watch("./src/templates/**/*.html")
+    .watch(["./src/templates/**/*.template.pug"])
     .on("change", gulp.series(build, browserSync.reload));
   //4 whatch js files changes in case you want to add js files
   //gulp.watch("./src/js/**/*.js").on("change", browserSync.reload);
